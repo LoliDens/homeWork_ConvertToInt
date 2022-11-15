c90e433150f7da257ad7ec7887b0ac128a056245
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-11-15 22:39:51 +0500

    Исправлен метод конверта

--- homeWork_ConvertToInt/Program.cs
+++ homeWork_ConvertToInt/Program.cs
@@ -10,25 +10,25 @@ namespace homeWork_ConvertToInt
     {
         static void Main(string[] args)
         {
-            string numberForConvert;
             int number;
 
-            Console.Write("Введите чило: ");
-            numberForConvert = Console.ReadLine();
-
-            number = TryConvert(numberForConvert);
+            number = TryConvert();
             Console.WriteLine($"Введенное вами число - {number}");
             Console.ReadKey();
         }
 
-        static int TryConvert(string number) 
+        static int TryConvert() 
         {
             int result;
+            string numberForConvert;
 
-            while (int.TryParse(number, out result) == false) 
+            Console.Write("Введите чило: ");
+            numberForConvert = Console.ReadLine();
+
+            while (int.TryParse(numberForConvert, out result) == false) 
             {
                 Console.Write("Число введено неверно\nВведите число повторно:");
-                number = Console.ReadLine();
+                numberForConvert = Console.ReadLine();
             }
 
             return result;
